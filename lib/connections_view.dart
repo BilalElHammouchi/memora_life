dff0ff046abe1de4df463dc87314fea5ed399edb
TheKillerBill <bilal-mok@hotmail.fr>
2023-06-17 17:44:30 +0100

    added barebones connections page

--- lib/connections_view.dart
+++ lib/connections_view.dart
@@ -1,15 +1,113 @@
 import 'package:flutter/material.dart';
 
-class ConnectionsView extends StatefulWidget {
-  const ConnectionsView({super.key});
+class ConnectionsPage extends StatefulWidget {
+  const ConnectionsPage({Key? key}) : super(key: key);
 
   @override
-  State<ConnectionsView> createState() => _ConnectionsViewState();
+  State<ConnectionsPage> createState() => _ConnectionsPageState();
 }
 
-class _ConnectionsViewState extends State<ConnectionsView> {
+class _ConnectionsPageState extends State<ConnectionsPage> {
+  List<String> connections = [];
+  List<String> buildings = [];
+  List<String> rooms = [];
+
+  void addConnection(String user) {
+    setState(() {
+      connections.add(user);
+    });
+  }
+
+  void addBuilding(String building) {
+    setState(() {
+      buildings.add(building);
+    });
+  }
+
+  void addRoom(String room) {
+    setState(() {
+      rooms.add(room);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Row(
+      children: [
+        Expanded(
+          child: Container(
+            color: Colors.white,
+            child: Column(
+              children: [
+                Icon(
+                  Icons.people,
+                  size: 100,
+                ),
+                const Text('Connections'),
+                ElevatedButton(
+                  onPressed: () {
+                    addConnection('New Connection');
+                  },
+                  child: const Text('Add Connection'),
+                ),
+                ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: connections.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(connections[index]),
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+        Expanded(
+          child: Container(
+            color: Colors.green,
+            child: Column(
+              children: [
+                Icon(
+                  Icons.house,
+                  size: 100,
+                ),
+                const Text('Buildings and Rooms'),
+                ElevatedButton(
+                  onPressed: () {
+                    addBuilding('New Building');
+                  },
+                  child: const Text('Add Building'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    addRoom('New Room');
+                  },
+                  child: const Text('Add Room'),
+                ),
+                ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: buildings.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(buildings[index]),
+                    );
+                  },
+                ),
+                ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: rooms.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(rooms[index]),
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
